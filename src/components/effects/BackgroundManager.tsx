438517d8a820b96f7e32ffa706859015a3ff0f5c
TaHa <tahanasrollahii@gmail.com>
2026-08-11 20:11:19 +0330

    remove planets

--- src/components/effects/BackgroundManager.tsx
+++ src/components/effects/BackgroundManager.tsx
@@ -1,17 +1,7 @@
 'use client';
 
-import { usePathname } from 'next/navigation';
 import GalaxyBackground from './GalaxyBackground';
-import SolarSystemJourney from './SolarSystemJourney';
 
 export default function BackgroundManager() {
-  const pathname = usePathname();
-
-  // Show the solar system journey only on the home page
-  if (pathname === '/') {
-    return <SolarSystemJourney />;
-  }
-
-  // Fallback to the standard galaxy background for all other pages
   return <GalaxyBackground />;
 }
